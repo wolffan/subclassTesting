4615bbf1c1fc87cfa4fedfdf7d870f6894ddfdcd
Raimon Lapuente <r.lapuente@gmail.com>
2015-01-25 23:05:37 +0000

    Empty project

---
+++ subclasTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  subclasTesting
+//
+//  Created by Raimon Lapuente on 25/1/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ subclasTesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  subclasTesting
+//
+//  Created by Raimon Lapuente on 25/1/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
